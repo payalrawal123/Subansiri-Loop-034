3b309f81fd1f2d0ca6a2d1187a9536453aa97b6b
payal <bharatrawal.br998877@gmail.com>
2024-05-09 18:26:55 +0530

    create imageuploader

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { connectDB } = require("./config/database");
 const { cloudinaryConnect } = require("./config/cloudinary");
 require("dotenv").config();
@@ -6,6 +9,20 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        // origin: 'http://localhost:5173', // frontend link
+        origin: "*",
+        credentials: true
+    })
+);
+app.use(
+  fileUpload({
+      useTempFiles: true,
+      tempFileDir: '/tmp'
+  })
+)
 
 app.listen(PORT, async (req, res) => {
   try {
