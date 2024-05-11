67a4a60910f7e82681342957fd25286f85ebfc18
Zeeshan6871 <furqanspn@gmail.com>
2024-05-12 00:42:28 +0530

    MainFile Updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducer/index";
+import { Toaster } from "react-hot-toast";
+import {} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <React.StrictMode>
+          <App />
+          <Toaster />
+        </React.StrictMode>
+      </Provider>
+    </BrowserRouter>
+  </ChakraProvider>
+);
